c51b2309beba641c896340a7e68939d3aaf5b6a3
fullhavocjosh <josh@rollet.family>
2026-08-19 16:49:25 -0400

    Remove overridebar condition from actionbar visibility

--- ElvUI/Settings/Profile.lua
+++ ElvUI/Settings/Profile.lua
@@ -5233,7 +5233,7 @@ P.actionbar = {
 			HUNTER = "[mod:ctrl,mod:shift] 4; [mod:ctrl] 2; [mod:shift] 3;",
 			MAGE = "[mod:ctrl,mod:shift] 4; [mod:ctrl] 2; [mod:shift] 3;"
 		},
-		visibility = "[vehicleui][overridebar][possessbar] hide; show"
+		visibility = "[vehicleui][possessbar] hide; show"
 	},
 	bar2 = {
 		enabled = false,
